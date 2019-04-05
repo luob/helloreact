f1babfe478d4cd88afca2192d04d114972804532
luob <contact@jiang-yan.com>
2019-04-05 20:34:38 +0800

    Updates

---
+++ precache-manifest.3d2551575dce81f3c5a13873243c5c7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6ba4c2fc472aa440e43",
+    "url": "/helloreact/static/css/main.58ec2723.chunk.css"
+  },
+  {
+    "revision": "c6ba4c2fc472aa440e43",
+    "url": "/helloreact/static/js/main.fdd7a95d.chunk.js"
+  },
+  {
+    "revision": "bca317f15aa14b5bbd5a",
+    "url": "/helloreact/static/js/runtime~main.166ef739.js"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/helloreact/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "6b8bfbf056ed5d88af50cec0a37cad5f",
+    "url": "/helloreact/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/helloreact/precache-manifest.1b1799bd9cf6b42f5b0b262ead7dbc57.js"
+  "/helloreact/precache-manifest.3d2551575dce81f3c5a13873243c5c7b.js"
 );
 
 workbox.clientsClaim();
