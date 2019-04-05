9f791e3440345184466653ec088977cd60ce768e
luob <contact@jiang-yan.com>
2019-04-05 18:30:44 +0800

    Updates

---
+++ precache-manifest.1b1799bd9cf6b42f5b0b262ead7dbc57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce73aba9e7863666f8fb",
+    "url": "/helloreact/static/css/main.a746e5f7.chunk.css"
+  },
+  {
+    "revision": "ce73aba9e7863666f8fb",
+    "url": "/helloreact/static/js/main.6d3e12f6.chunk.js"
+  },
+  {
+    "revision": "bca317f15aa14b5bbd5a",
+    "url": "/helloreact/static/js/runtime~main.166ef739.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/helloreact/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/helloreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7606f8e2a02bab9cf31492cdd70c07d0",
+    "url": "/helloreact/index.html"
+  }
+];
